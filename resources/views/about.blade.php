703021e3cd820c791bd0c019f7c68f6a6f85bde7
yasmeen_alqady <yasmeenalqady117@gmail.com>
2025-09-01 09:19:32 +0300

    inital project setup

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello Page</title>
+</head>
+<body>
+    <h1>مرحبا ياسمين 🎉</h1>
+    <p>هذه أول صفحة Laravel مع Route مخصص.</p>
+</body>
+</html>
