6451f9b0e641ca7db7e51655231d47f7857f6139
yasmeen_alqady <yasmeenalqady117@gmail.com>
2025-09-03 23:01:32 +0300

    feat: Add frontend index page, assets, and update routes

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-    
+    return view('frontend.index');
 });
-Route::get('about', function () {
-    return view('about');
-    
-});
+
